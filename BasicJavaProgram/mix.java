d61bd44384343198dad754e8a894f9d1d0de03f9
Sunil Jadhav <sj368581@gmail.com>
2026-01-07 17:43:13 +0530

    Add files via upload

---
+++ BasicJavaProgram/mix.java
@@ -0,0 +1,18 @@
+package basicprogram;
+
+public class mix {
+
+	public static void main(String[] args) {
+		
+		 int a = 10;
+	        int b = 20;
+
+	        if (a > b) {
+	            System.out.println("Max number is: " + a);
+	        } else {
+	            System.out.println("Max number is: " + b);
+	        }
+	    
+	}
+
+}
